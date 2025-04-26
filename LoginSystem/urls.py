18e80cb4dcc3497d6f81798812096f936867b75b
abhiram7605 <abhiram7605@gmail.com>
2025-04-26 16:19:19 -0700

    This commit is for task-2

--- LoginSystem/urls.py
+++ LoginSystem/urls.py
@@ -1,3 +1,4 @@
+from django.urls import include, path
 """
 URL configuration for LoginSystem project.
 
@@ -19,4 +20,5 @@ from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("loginify/", include("Loginify.urls"))
 ]
